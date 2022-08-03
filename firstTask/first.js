6e05ff2b9094cf1c39f199c13d5705f84cc74c6a
tskorohod <tskorohod@lmteam.by>
2022-08-03 22:26:32 +0300

    finished first task

--- firstTask/first.js
+++ firstTask/first.js
@@ -1,5 +1,13 @@
+/**Задача: при передаче в функцию какого-либо числа, выводить все числа, на которое делится передаваемое число без остатка*/
 const divisors = (num) => {
-	console.log(num);
+	const arr = [];
+	const unSuit = [];
+	let i;
+	for(i = 1; i <= num; i++) {
+		let suit = num % i;
+		suit === 0 ? arr.push(i) : unSuit.push(i);
+	}
+	console.log(arr);
 };
 
-divisors(10);
+divisors(16);
